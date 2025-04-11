2744221caaa6111ddaf039411fd3b5879f377940
Malay Dewari <dewarimalay@gmail.com>
2025-04-11 10:13:01 +0530

    second commit

--- src/app/dashboard/[slug]/page.tsx
+++ src/app/dashboard/[slug]/page.tsx
@@ -1,7 +1,14 @@
+import { Counter } from "@/app/components/Counter";
+
 type Params = Promise<{slug: string}>;
 
 export default async function DashboardRoute({params} : {params: Params}) {
 
     const {slug} = await params
-    return <h1>hello from the slug route : {slug}</h1>
+    return (
+        <>
+            <h1>hello from the slug: {slug}</h1>
+            <Counter/>
+        </>
+    )
 }
